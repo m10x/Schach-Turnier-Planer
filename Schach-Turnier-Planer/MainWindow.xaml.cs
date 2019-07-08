e80e222022a7876af89018f2f237fcca43e74f83
Maximilian Hildebrand <maximilian.hildebrand@gmx.de>
2019-07-08 13:37:55 +0200

    2nd

---
+++ Schach-Turnier-Planer/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Schach_Turnier_Planer
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public Team Teams;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TeamAddButton_Click(object sender, RoutedEventArgs e)
+        {
+            Teams = new Team(textTeamName.Text, textTeamStadt.Text, textTeamPartner.Text, System.Convert.ToInt32(textTeamNummer.Text));
+            lbTeams.Items.Add(Teams);
+        }
+
+        private void TeamRemoveButton_Click(object sender, RoutedEventArgs e)
+        {
+            lbTeams.Items.Remove(lbTeams.SelectedItem);
+        }
+
+        private void TeamChangeButton_Click(object sender, RoutedEventArgs e)
+        {
+            Teams = lbTeams.SelectedItem as Team;
+            textTeamName.Text = Teams.Name;
+            textTeamStadt.Text = Teams.Stadt;
+            textTeamPartner.Text = Teams.Partner;
+            textTeamNummer.Text = Teams.Nummer.ToString();
+        }
+    }
+}
---
+++ Schach-Turnier-Planer/Team.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Schach_Turnier_Planer
+{
+    public class Team
+    {
+        public Team (string Name, string Stadt, string Partner, int Nummer)
+        {
+            this.Name = Name;
+            this.Stadt = Stadt;
+            this.Partner = Partner;
+            this.Nummer = Nummer;
+        }
+        public string Name { get; set; }
+        public string Stadt { get; set; }
+        public string Partner { get; set; }
+        public int Nummer { get; set; }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
